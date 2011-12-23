65138705bad978e47b38990a1c0dec698cb2f73d
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-12-23 13:59:05 -0800

    run as anonymous if no authentication was performe

--- src/main/java/org/jenkinsci/main/modules/sshd/CLICommandAdapter.java
+++ src/main/java/org/jenkinsci/main/modules/sshd/CLICommandAdapter.java
@@ -2,6 +2,7 @@ package org.jenkinsci.main.modules.sshd;
 
 import hudson.Extension;
 import hudson.cli.CLICommand;
+import jenkins.model.Jenkins;
 import org.apache.sshd.server.Command;
 
 import java.io.PrintStream;
@@ -23,8 +24,11 @@ public class CLICommandAdapter extends SshCommandFactory {
         return new AsynchronousCommand(commandLine) {
             @Override
             protected int run() throws Exception {
-                // TODO: is there any way to learn the locale of the client?
-                c.setTransportAuth(getCurrentUser().impersonate());
+                // run as the authenticated user if we've actually authenticated the user,
+                // or otherwise run as anonymous
+                if (Jenkins.getInstance().isUseSecurity())
+                    c.setTransportAuth(getCurrentUser().impersonate());
+
                 CommandLine cmds = getCmdLine();
                 return c.main(cmds.subList(1,cmds.size()), Locale.getDefault(), getInputStream(), new PrintStream(getOutputStream()), new PrintStream(getErrorStream()));
             }
