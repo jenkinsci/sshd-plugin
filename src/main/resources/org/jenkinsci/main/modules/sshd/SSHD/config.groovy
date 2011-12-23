fbc2b9ad0dc5eebc37e3fa226dd70ae73f20a491
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-12-23 15:57:03 -0800

    adding a section

--- src/main/resources/org/jenkinsci/main/modules/sshd/SSHD/config.groovy
+++ src/main/resources/org/jenkinsci/main/modules/sshd/SSHD/config.groovy
@@ -2,6 +2,8 @@ package org.jenkinsci.main.modules.sshd.SSHD;
 
 def f=namespace(lib.FormTagLib)
 
-f.entry(title:_("SSHD Port"),field:"port") {
-    f.serverTcpPort()
+f.section(title:_("SSH Server")) {
+    f.entry(title:_("SSHD Port"),field:"port") {
+        f.serverTcpPort()
+    }
 }
