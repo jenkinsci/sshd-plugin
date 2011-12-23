10b1b8b55dc9f46670a021b4faff7746ba5664ec
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-12-23 13:56:07 -0800

    running the command with the right identity

--- src/main/java/org/jenkinsci/main/modules/sshd/BaseCLICommandAdapter.java
+++ src/main/java/org/jenkinsci/main/modules/sshd/BaseCLICommandAdapter.java
@@ -1,14 +1,15 @@
 package org.jenkinsci.main.modules.sshd;
 
 import hudson.Extension;
-import hudson.cli.BaseCLICommand;
+import hudson.cli.CLICommand;
+import hudson.model.User;
 import org.apache.sshd.server.Command;
 
 import java.io.PrintStream;
 import java.util.Locale;
 
 /**
- * {@link SshCommandFactory} that invokes {@link BaseCLICommand}s.
+ * {@link SshCommandFactory} that invokes {@link CLICommand}s.
  *
  * @author Kohsuke Kawaguchi
  */
@@ -17,14 +18,16 @@ public class BaseCLICommandAdapter extends SshCommandFactory {
     @Override
     public Command create(CommandLine commandLine) {
         String cmd = commandLine.get(0);
-        final BaseCLICommand c = BaseCLICommand.clone(cmd);
+        final CLICommand c = CLICommand.clone(cmd);
         if (c==null)        return null;    // no such command
 
         return new AsynchronousCommand(commandLine) {
             @Override
             protected int run() throws Exception {
                 // TODO: is there any way to learn the locale of the client?
-                return c.main(getCmdLine(), Locale.getDefault(), getInputStream(), new PrintStream(getOutputStream()), new PrintStream(getErrorStream()));
+                c.setTransportAuth(getCurrentUser().impersonate());
+                CommandLine cmds = getCmdLine();
+                return c.main(cmds.subList(1,cmds.size()), Locale.getDefault(), getInputStream(), new PrintStream(getOutputStream()), new PrintStream(getErrorStream()));
             }
         };
     }
