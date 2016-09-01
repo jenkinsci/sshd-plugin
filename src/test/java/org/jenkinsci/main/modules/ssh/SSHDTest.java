61a83734261be31d71c42b670c6e251f63e16745
Oleg Nenashev <o.v.nenashev@gmail.com>
2016-09-01 14:21:25 +0200

    Migrate the SSHDTest to JenkinsRule

--- src/test/java/org/jenkinsci/main/modules/ssh/SSHDTest.java
+++ src/test/java/org/jenkinsci/main/modules/ssh/SSHDTest.java
@@ -1,27 +1,37 @@
 package org.jenkinsci.main.modules.ssh;
 
+import org.hamcrest.CoreMatchers;
 import org.jenkinsci.main.modules.sshd.SSHD;
-import org.jvnet.hudson.test.HudsonTestCase;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
 
 import javax.inject.Inject;
 
+import static org.junit.Assert.assertThat;
+
 /**
+ * Tests of {@link SSHD}.
  * @author Kohsuke Kawaguchi
  */
-public class SSHDTest extends HudsonTestCase {
+public class SSHDTest {
     @Inject
     SSHD sshd;
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
 
     /**
      * Makes sure 3 mode of the value round-trips correctly
      */
-    public void testConfig() throws Exception {
-        jenkins.getInjector().injectMembers(this);
+    @Test
+    public void configRoundtrip() throws Exception {
+        j.jenkins.getInjector().injectMembers(this);
 
         for (int i : new int[]{-1,0,100}) {
             sshd.setPort(i);
-            configRoundtrip();
-            assertEquals(i,sshd.getPort());
+            j.configRoundtrip();
+            assertThat("SSHD has not been allocated to the specified port", sshd.getPort(), CoreMatchers.equalTo(i));
         }
     }
 }
