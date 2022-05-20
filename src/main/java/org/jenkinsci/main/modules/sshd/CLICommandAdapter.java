883d165ac1d3f6729a2e38c0fd10f120eee02099
Allan Burdajewicz <aburdajewicz@cloudbees.com>
2022-05-21 00:17:37 +1000

    [JENKINS-68541] Prevent stdout/stderr stream from being closed by invoked CLI commands (#74)
    * [JENKINS-68541] Prevent stdout/stderr stream from being closed by invoked CLI commands
    
    * [JENKINS-68541] Add test
    
    * [JENKINS-68541] Add a todo for SSHD-1257

--- src/main/java/org/jenkinsci/main/modules/sshd/CLICommandAdapter.java
+++ src/main/java/org/jenkinsci/main/modules/sshd/CLICommandAdapter.java
@@ -1,13 +1,15 @@
 package org.jenkinsci.main.modules.sshd;
 
+import hudson.CloseProofOutputStream;
 import hudson.Extension;
 import hudson.cli.CLICommand;
 import hudson.model.User;
+import org.apache.sshd.server.command.Command;
+
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.Charset;
 import java.util.Locale;
-import org.apache.sshd.server.command.Command;
 
 /**
  * {@link SshCommandFactory} that invokes {@link CLICommand}s.
@@ -33,9 +35,10 @@ public class CLICommandAdapter extends SshCommandFactory {
                 CommandLine cmds = getCmdLine();
 
                 //TODO: Consider switching to UTF-8
+                //TODO: Consider removing the CloseProofOutputStream wrapper when SSHD-1257 is available
                 return c.main(cmds.subList(1,cmds.size()), Locale.getDefault(), getInputStream(),
-                        new PrintStream(getOutputStream(), false, Charset.defaultCharset().toString()),
-                        new PrintStream(getErrorStream(), false, Charset.defaultCharset().toString()));
+                        new PrintStream(new CloseProofOutputStream(getOutputStream()), false, Charset.defaultCharset().toString()),
+                        new PrintStream(new CloseProofOutputStream(getErrorStream()), false, Charset.defaultCharset().toString()));
             }
         };
     }
