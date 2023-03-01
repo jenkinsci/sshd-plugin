b88e16d97f674a5432e844e913cd91708ee06161
Mark Waite <mark.earl.waite@gmail.com>
2023-03-01 14:53:36 -0500

    Remove unused Java imports (#125)

--- src/test/java/org/jenkinsci/main/modules/cli/auth/ssh/CLITest.java
+++ src/test/java/org/jenkinsci/main/modules/cli/auth/ssh/CLITest.java
@@ -74,7 +74,6 @@ import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
