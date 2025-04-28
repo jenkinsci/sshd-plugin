7ada0b581d3a86d931c6c5743c4ec3aa2001353a
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-04-28 16:46:45 +0200

    Migrate tests to JUnit5
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup

--- src/test/java/org/jenkinsci/main/modules/cli/auth/ssh/CLITest.java
+++ src/test/java/org/jenkinsci/main/modules/cli/auth/ssh/CLITest.java
@@ -24,7 +24,6 @@
 
 package org.jenkinsci.main.modules.cli.auth.ssh;
 
-import org.htmlunit.WebResponse;
 import hudson.Extension;
 import hudson.Launcher;
 import hudson.Proc;
@@ -34,33 +33,33 @@ import hudson.model.UnprotectedRootAction;
 import hudson.model.User;
 import hudson.security.csrf.CrumbExclusion;
 import hudson.util.StreamTaskListener;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jenkins.model.GlobalConfiguration;
 import jenkins.model.Jenkins;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.output.TeeOutputStream;
 import org.apache.sshd.common.util.io.ModifiableFileWatcher;
+import org.htmlunit.WebResponse;
 import org.jenkinsci.main.modules.sshd.SSHD;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-import org.jvnet.hudson.test.BuildWatcher;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.MockAuthorizationStrategy;
 import org.jvnet.hudson.test.SleepBuilder;
 import org.jvnet.hudson.test.TestExtension;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 import org.kohsuke.stapler.HttpResponses.HttpResponseException;
 import org.kohsuke.stapler.Stapler;
 import org.kohsuke.stapler.StaplerProxy;
 import org.kohsuke.stapler.StaplerRequest2;
 import org.kohsuke.stapler.StaplerResponse2;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
@@ -74,35 +73,36 @@ import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assume.assumeNoException;
-import static org.junit.Assume.assumeThat;
-import static org.junit.Assume.assumeTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 /**
  * Tests CLI operation with the classes in the {@code org.jenkinsci.main.modules.sshd} package.
  * Extracted from https://github.com/jenkinsci/jenkins/blob/e681b4ff928435a500dcfb372cc9fc15b1c94413/test/src/test/java/hudson/cli/CLITest.java
  */
-public class CLITest {
+@WithJenkins
+class CLITest {
 
-    @ClassRule
-    public static BuildWatcher buildWatcher = new BuildWatcher();
-    
-    @Rule
-    public JenkinsRule r = new JenkinsRule();
-
-    @Rule
-    public TemporaryFolder tmp = new TemporaryFolder();
+    @TempDir
+    private File tmp;
 
     private File home;
     private File jar;
 
-    /** Sets up a fake {@code user.home} so that tests {@code -ssh} mode does not get confused by the developer’s real {@code ~/.ssh/known_hosts}. */
+    private JenkinsRule r;
+
+    @BeforeEach
+    void setUp(JenkinsRule rule) {
+        r = rule;
+    }
+
+    /**
+     * Sets up a fake {@code user.home} so that tests {@code -ssh} mode does not get confused by the developer’s real {@code ~/.ssh/known_hosts}.
+     */
     private File tempHome() throws IOException {
-        home = tmp.newFolder();
+        home = newFolder(tmp, "junit");
         // Seems it gets created automatically but with inappropriate permissions:
         File known_hosts = new File(new File(home, ".ssh"), "known_hosts");
         assumeTrue(known_hosts.getParentFile().mkdir());
@@ -112,16 +112,16 @@ public class CLITest {
         try {
             Files.getOwner(known_hosts.toPath());
         } catch (IOException x) {
-            assumeNoException("Sometimes on Windows KnownHostsServerKeyVerifier.acceptIncompleteHostKeys says WARNING: Failed (FileSystemException) to reload server keys from …\\\\.ssh\\\\known_hosts: … Incorrect function.", x);
+            assumeTrue(false, "Sometimes on Windows KnownHostsServerKeyVerifier.acceptIncompleteHostKeys says WARNING: Failed (FileSystemException) to reload server keys from …\\\\.ssh\\\\known_hosts: … Incorrect function. Error: " + x);
         }
-        assumeThat("or on Windows DefaultKnownHostsServerKeyVerifier.reloadKnownHosts says invalid file permissions: Owner violation (Administrators)",
-            ModifiableFileWatcher.validateStrictConfigFilePermissions(known_hosts.toPath()), nullValue());
+        assumeTrue(ModifiableFileWatcher.validateStrictConfigFilePermissions(known_hosts.toPath()) == null,
+                "or on Windows DefaultKnownHostsServerKeyVerifier.reloadKnownHosts says invalid file permissions: Owner violation (Administrators)");
         return home;
     }
 
     @Issue("JENKINS-41745")
     @Test
-    public void strictHostKey() throws Exception {
+    void strictHostKey() throws Exception {
         Launcher.LocalLauncher localLauncher = new Launcher.LocalLauncher(StreamTaskListener.fromStderr());
         String jenkinsUrl = r.getURL().toString();
         home = tempHome();
@@ -131,39 +131,39 @@ public class CLITest {
         r.jenkins.setSecurityRealm(r.createDummySecurityRealm());
         r.jenkins.setAuthorizationStrategy(new MockAuthorizationStrategy().grant(Jenkins.ADMINISTER).everywhere().to("admin"));
         SSHD.get().setPort(0);
-        File privkey = tmp.newFile("id_rsa");
+        File privkey = File.createTempFile("id_rsa", null, tmp);
         FileUtils.copyURLToFile(CLITest.class.getResource("id_rsa"), privkey);
         User.getById("admin", true).addProperty(new UserPropertyImpl(IOUtils.toString(CLITest.class.getResource("id_rsa.pub"), StandardCharsets.UTF_8)));
         assertNotEquals(0, new Launcher.LocalLauncher(StreamTaskListener.fromStderr()).launch().cmds(
-            "java", "-Duser.home=" + home, "-jar", jar.getAbsolutePath(), "-s", r.getURL().toString(), "-ssh", "-user", "admin", "-i", privkey.getAbsolutePath(), "-strictHostKey", "who-am-i"
+                "java", "-Duser.home=" + home, "-jar", jar.getAbsolutePath(), "-s", r.getURL().toString(), "-ssh", "-user", "admin", "-i", privkey.getAbsolutePath(), "-strictHostKey", "who-am-i"
         ).stdout(System.out).stderr(System.err).join());
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         assertEquals(0, new Launcher.LocalLauncher(StreamTaskListener.fromStderr()).launch().cmds(
-            "java", "-Duser.home=" + home, "-jar", jar.getAbsolutePath(), "-s", r.getURL().toString(), "-ssh", "-user", "admin", "-i", privkey.getAbsolutePath(), "-logger", "FINEST",  "who-am-i"
+                "java", "-Duser.home=" + home, "-jar", jar.getAbsolutePath(), "-s", r.getURL().toString(), "-ssh", "-user", "admin", "-i", privkey.getAbsolutePath(), "-logger", "FINEST", "who-am-i"
         ).stdout(baos).stderr(System.err).join());
         assertThat(baos.toString(), containsString("Authenticated as: admin"));
         baos = new ByteArrayOutputStream();
         assertEquals(0, new Launcher.LocalLauncher(StreamTaskListener.fromStderr()).launch().cmds(
-            "java", "-Duser.home=" + home, "-jar", jar.getAbsolutePath(), "-s", r.getURL().toString()./* just checking */replaceFirst("/$", ""), "-ssh", "-user", "admin", "-i", privkey.getAbsolutePath(), "-strictHostKey", "who-am-i"
+                "java", "-Duser.home=" + home, "-jar", jar.getAbsolutePath(), "-s", r.getURL().toString()./* just checking */replaceFirst("/$", ""), "-ssh", "-user", "admin", "-i", privkey.getAbsolutePath(), "-strictHostKey", "who-am-i"
         ).stdout(baos).stderr(System.err).join());
         assertThat(baos.toString(), containsString("Authenticated as: admin"));
     }
 
     private void grabCliJar() throws IOException {
-        jar = tmp.newFile("jenkins-cli.jar");
+        jar = File.createTempFile("jenkins-cli.jar", null, tmp);
         FileUtils.copyURLToFile(r.jenkins.getJnlpJars("jenkins-cli.jar").getURL(), jar);
     }
 
     @Issue("JENKINS-41745")
     @Test
-    public void interrupt() throws Exception {
+    void interrupt() throws Exception {
         home = tempHome();
         grabCliJar();
 
         r.jenkins.setSecurityRealm(r.createDummySecurityRealm());
         r.jenkins.setAuthorizationStrategy(new MockAuthorizationStrategy().grant(Jenkins.ADMINISTER).everywhere().to("admin"));
         SSHD.get().setPort(0);
-        File privkey = tmp.newFile("id_rsa");
+        File privkey = File.createTempFile("id_rsa", null, tmp);
         FileUtils.copyURLToFile(CLITest.class.getResource("id_rsa"), privkey);
         User.getById("admin", true).addProperty(new UserPropertyImpl(IOUtils.toString(CLITest.class.getResource("id_rsa.pub"), StandardCharsets.UTF_8)));
         FreeStyleProject p = r.createFreeStyleProject("p");
@@ -184,28 +184,29 @@ public class CLITest {
 
     @Issue("JENKINS-68541")
     @Test
-    public void outputStream() throws Exception {
+    void outputStream() throws Exception {
         home = tempHome();
         grabCliJar();
 
         r.jenkins.setSecurityRealm(r.createDummySecurityRealm());
         r.jenkins.setAuthorizationStrategy(new MockAuthorizationStrategy().grant(Jenkins.ADMINISTER).everywhere().to("admin"));
         SSHD.get().setPort(0);
-        File privkey = tmp.newFile("id_rsa");
+        File privkey = File.createTempFile("id_rsa", null, tmp);
         FileUtils.copyURLToFile(CLITest.class.getResource("id_rsa"), privkey);
         User.getById("admin", true).addProperty(new UserPropertyImpl(IOUtils.toString(CLITest.class.getResource("id_rsa.pub"), StandardCharsets.UTF_8)));
         StreamTaskListener stl = StreamTaskListener.fromStderr();
         List<String> args = Arrays.asList("java", "-Duser.home=" + home, "-jar", jar.getAbsolutePath(), "-s", r.getURL().toString(), "-ssh", "-user", "admin", "-i", privkey.getAbsolutePath(), "close-stdout-stream");
         int ret = new Launcher.LocalLauncher(stl).launch().cmds(args)
-            .stdout(System.out)
-            .stderr(System.err)
-            .start()
-            .joinWithTimeout(5, TimeUnit.SECONDS, stl);
+                .stdout(System.out)
+                .stderr(System.err)
+                .start()
+                .joinWithTimeout(5, TimeUnit.SECONDS, stl);
         assertEquals(0, ret);
     }
 
-    @Test @Issue("JENKINS-44361")
-    public void reportNotJenkins() throws Exception {
+    @Test
+    @Issue("JENKINS-44361")
+    void reportNotJenkins() throws Exception {
         home = tempHome();
         grabCliJar();
 
@@ -218,22 +219,27 @@ public class CLITest {
         assertThat(baos.toString(), containsString("There's no Jenkins running at"));
         assertNotEquals(0, ret);
     }
+
     @TestExtension("reportNotJenkins")
     public static final class NoJenkinsAction extends CrumbExclusion implements UnprotectedRootAction, StaplerProxy {
 
-        @Override public String getIconFileName() {
+        @Override
+        public String getIconFileName() {
             return "not-jenkins";
         }
 
-        @Override public String getDisplayName() {
+        @Override
+        public String getDisplayName() {
             return "not-jenkins";
         }
 
-        @Override public String getUrlName() {
+        @Override
+        public String getUrlName() {
             return "not-jenkins";
         }
 
-        @Override public Object getTarget() {
+        @Override
+        public Object getTarget() {
             doDynamic(Stapler.getCurrentRequest2(), Stapler.getCurrentResponse2());
             return this;
         }
@@ -249,8 +255,9 @@ public class CLITest {
         }
     }
 
-    @Test @Issue("JENKINS-44361")
-    public void redirectToEndpointShouldBeFollowed() throws Exception {
+    @Test
+    @Issue("JENKINS-44361")
+    void redirectToEndpointShouldBeFollowed() throws Exception {
         home = tempHome();
         grabCliJar();
 
@@ -263,12 +270,12 @@ public class CLITest {
         JenkinsRule.WebClient wc = r.createWebClient()
                 .withRedirectEnabled(false)
                 .withThrowExceptionOnFailingStatusCode(false);
-        
+
         WebResponse rsp = wc.goTo("cli-proxy/").getWebResponse();
-        assertEquals(rsp.getContentAsString(), HttpURLConnection.HTTP_MOVED_TEMP, rsp.getStatusCode());
-        assertNull(rsp.getContentAsString(), rsp.getResponseHeaderValue("X-Jenkins"));
-        assertNull(rsp.getContentAsString(), rsp.getResponseHeaderValue("X-Jenkins-CLI-Port"));
-        assertNull(rsp.getContentAsString(), rsp.getResponseHeaderValue("X-SSH-Endpoint"));
+        assertEquals(HttpURLConnection.HTTP_MOVED_TEMP, rsp.getStatusCode(), rsp.getContentAsString());
+        assertNull(rsp.getResponseHeaderValue("X-Jenkins"), rsp.getContentAsString());
+        assertNull(rsp.getResponseHeaderValue("X-Jenkins-CLI-Port"), rsp.getContentAsString());
+        assertNull(rsp.getResponseHeaderValue("X-SSH-Endpoint"), rsp.getContentAsString());
 
         String url = r.getURL().toString() + "cli-proxy/";
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -284,19 +291,23 @@ public class CLITest {
     @TestExtension("redirectToEndpointShouldBeFollowed")
     public static final class CliProxyAction extends CrumbExclusion implements UnprotectedRootAction, StaplerProxy {
 
-        @Override public String getIconFileName() {
+        @Override
+        public String getIconFileName() {
             return "cli-proxy";
         }
 
-        @Override public String getDisplayName() {
+        @Override
+        public String getDisplayName() {
             return "cli-proxy";
         }
 
-        @Override public String getUrlName() {
+        @Override
+        public String getUrlName() {
             return "cli-proxy";
         }
 
-        @Override public Object getTarget() {
+        @Override
+        public Object getTarget() {
             throw doDynamic(Stapler.getCurrentRequest2(), Stapler.getCurrentResponse2());
         }
 
@@ -305,7 +316,7 @@ public class CLITest {
             // Custom written redirect so no traces of Jenkins are present in headers
             return new HttpResponseException() {
                 @Override
-                public void generateResponse(StaplerRequest2 req, StaplerResponse2 rsp, Object node) throws IOException, ServletException {
+                public void generateResponse(StaplerRequest2 req, StaplerResponse2 rsp, Object node) throws IOException {
                     rsp.setHeader("Location", url);
                     rsp.setContentType("text/html");
                     rsp.setStatus(HttpURLConnection.HTTP_MOVED_TEMP);
@@ -335,5 +346,14 @@ public class CLITest {
             stdout.close();
             return 0;
         }
+    }
+
+    private static File newFolder(File root, String... subDirs) throws IOException {
+        String subFolder = String.join("/", subDirs);
+        File result = new File(root, subFolder);
+        if (!result.mkdirs()) {
+            throw new IOException("Couldn't create folders " + root);
+        }
+        return result;
     }
 }
--- src/test/java/org/jenkinsci/main/modules/sshd/SSHDTest.java
+++ src/test/java/org/jenkinsci/main/modules/sshd/SSHDTest.java
@@ -3,14 +3,6 @@ package org.jenkinsci.main.modules.sshd;
 import hudson.Functions;
 import hudson.security.AbstractPasswordBasedSecurityRealm;
 import hudson.security.GroupDetails;
-import org.springframework.security.authentication.AccountExpiredException;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.authentication.CredentialsExpiredException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.LockedException;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.apache.sshd.client.SshClient;
 import org.apache.sshd.client.future.ConnectFuture;
 import org.apache.sshd.client.keyverifier.AcceptAllServerKeyVerifier;
@@ -20,11 +12,19 @@ import org.apache.sshd.common.SshException;
 import org.apache.sshd.common.cipher.Cipher;
 import org.jenkinsci.main.modules.cli.auth.ssh.PublicKeySignatureWriter;
 import org.jenkinsci.main.modules.cli.auth.ssh.UserPropertyImpl;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -34,22 +34,25 @@ import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 /**
  * Tests of {@link SSHD}.
+ *
  * @author Kohsuke Kawaguchi
  */
-public class SSHDTest {
+@WithJenkins
+class SSHDTest {
 
-    @Rule
-    public JenkinsRule j = new JenkinsRule();
+    private JenkinsRule r;
 
-    @Before
-    public void setUp() {
+    @BeforeEach
+    void setUp(JenkinsRule rule) {
+        r = rule;
         assumeFalse(Functions.isWindows());
     }
 
@@ -57,30 +60,30 @@ public class SSHDTest {
      * Makes sure 3 mode of the value round-trips correctly
      */
     @Test
-    public void configRoundtrip() throws Exception {
+    void configRoundtrip() throws Exception {
         SSHD sshd = SSHD.get();
 
-        for (int i : new int[]{-1,0,100}) {
+        for (int i : new int[]{-1, 0, 100}) {
             sshd.setPort(i);
-            j.configRoundtrip();
-            assertEquals("SSHD has not been allocated to the specified port", sshd.getPort(), i);
+            r.configRoundtrip();
+            assertEquals(sshd.getPort(), i, "SSHD has not been allocated to the specified port");
         }
     }
 
     @Test
     @Issue("JENKINS-39738")
-    public void checkActivatedCiphers() throws Exception {
+    void checkActivatedCiphers() {
         // Just ensure the method does not blow up && that at least one Cipher is available
         List<NamedFactory<Cipher>> activatedCiphers = SSHD.getActivatedCiphers();
-        assertTrue("At least one cipher should be activated", activatedCiphers.size() >= 1);
+        assertFalse(activatedCiphers.isEmpty(), "At least one cipher should be activated");
     }
 
     @Test
     @Issue("JENKINS-55813")
-    public void enabledUserShouldBeAuthorized() throws Exception {
+    void enabledUserShouldBeAuthorized() throws Exception {
         hudson.model.User enabled = hudson.model.User.getOrCreateByIdOrFullName("enabled");
         KeyPair keyPair = generateKeys(enabled);
-        j.jenkins.setSecurityRealm(new InvalidUserTypesRealm());
+        r.jenkins.setSecurityRealm(new InvalidUserTypesRealm());
         SSHD server = SSHD.get();
         server.setPort(0);
         server.start();
@@ -97,32 +100,32 @@ public class SSHDTest {
 
     @Test
     @Issue("JENKINS-55813")
-    public void disabledUserShouldBeUnauthorized() throws Exception {
+    void disabledUserShouldBeUnauthorized() throws Exception {
         assertUserCannotLoginToSSH("disabled");
     }
 
     @Test
     @Issue("JENKINS-55813")
-    public void lockedUserShouldBeUnauthorized() throws Exception {
+    void lockedUserShouldBeUnauthorized() throws Exception {
         assertUserCannotLoginToSSH("locked");
     }
 
     @Test
     @Issue("JENKINS-55813")
-    public void expiredUserShouldBeUnauthorized() throws Exception {
+    void expiredUserShouldBeUnauthorized() throws Exception {
         assertUserCannotLoginToSSH("expired");
     }
 
     @Test
     @Issue("JENKINS-55813")
-    public void passwordExpiredUserShouldBeUnauthorized() throws Exception {
+    void passwordExpiredUserShouldBeUnauthorized() throws Exception {
         assertUserCannotLoginToSSH("password_expired");
     }
 
     private void assertUserCannotLoginToSSH(String username) throws Exception {
         hudson.model.User user = hudson.model.User.getOrCreateByIdOrFullName(username);
         KeyPair keyPair = generateKeys(user);
-        j.jenkins.setSecurityRealm(new InvalidUserTypesRealm());
+        r.jenkins.setSecurityRealm(new InvalidUserTypesRealm());
 
         SSHD server = SSHD.get();
         server.setPort(0);
@@ -158,22 +161,13 @@ public class SSHDTest {
 
         @Override
         public UserDetails loadUserByUsername2(String user) throws UsernameNotFoundException {
-            switch (user) {
-                case "disabled":
-                    throw new DisabledException(user);
-
-                case "expired":
-                    throw new AccountExpiredException(user);
-
-                case "password_expired":
-                    throw new CredentialsExpiredException(user);
-
-                case "locked":
-                    throw new LockedException(user);
-
-                default:
-                    return new User(user, "", true, true, true, true, List.of());
-            }
+            return switch (user) {
+                case "disabled" -> throw new DisabledException(user);
+                case "expired" -> throw new AccountExpiredException(user);
+                case "password_expired" -> throw new CredentialsExpiredException(user);
+                case "locked" -> throw new LockedException(user);
+                default -> new User(user, "", true, true, true, true, List.of());
+            };
         }
 
         @Override
